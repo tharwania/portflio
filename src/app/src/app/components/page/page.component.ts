4f76e4e9744725d29d4250c15e5f0037347977e1
Avinash <tharwania@gmail.com>
2023-12-03 23:00:12 +0000

    added editor

--- src/app/src/app/components/page/page.component.ts
+++ src/app/src/app/components/page/page.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Route } from '@angular/router';
 import { DocumentData } from 'firebase/firestore';
 import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/services/auth.service';
 import { Page, PageDataService } from 'src/app/services/page-data.service';
-
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { ChangeEvent } from '@ckeditor/ckeditor5-angular/ckeditor.component';
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
@@ -11,16 +13,35 @@ import { Page, PageDataService } from 'src/app/services/page-data.service';
 })
 export class PageComponent implements OnInit {
   page: Observable<Page> | null = null;
-  constructor(private pageService: PageDataService, private route: ActivatedRoute) {
+  data: string = '';
+  isLoggedIn = false;
+  editMode = false;
+  public Editor = ClassicEditor;
+  constructor(private pageService: PageDataService,
+    private route: ActivatedRoute,
+    public readonly authService: AuthService) {
     this.route.paramMap.subscribe(paramMap => {
       const id = paramMap.get('id');
       if (id) {
-        this.page = pageService.getPage(id)
+        this.page = this.pageService.getPage(id)
       }
-      else {
-        // somethign default
-      }
+      authService.AuthState().subscribe(user => {
+        this.isLoggedIn = !!user;
+      })
     })
+  }
+  public onChange({ editor }: ChangeEvent) {
+    this.data = editor.data.get();
+  }
+
+  addOrUpdatePage() {
+    if (this.page) {
+      this.page.subscribe(page => {
+        page.content = this.data;
+        this.pageService.updateDoc(page);
+        this.editMode = false;
+      })
+    }
   }
 
   ngOnInit(): void {
